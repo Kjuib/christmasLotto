69c42d871228b9a3bba303d6b5a8f9b5e2387087
hbillings <hbillings@cmsonline.com>
2021-11-30 09:22:31 -0700

    removed database of sensitive info

--- src/db/employees.js
+++
@@ -1,5 +0,0 @@
-export const list = [
-    { id: 'e001', name: 'Heber Billings', image: '000.jpg' },
-    { id: 'e002', name: 'Kevin Batchelor', image: '000.jpg' },
-    { id: 'e003', name: 'Todd Risenmay', image: '000.jpg' },
-];
--- src/db/gifts.js
+++
@@ -1,5 +0,0 @@
-export const list = [
-    { id: 'g001', name: 'Big Lego Set', image: '000.jpg' },
-    { id: 'g002', name: 'Eye Dr Visit', image: '000.jpg' },
-    { id: 'g003', name: 'Dancing Lessons', image: '000.jpg' },
-];
